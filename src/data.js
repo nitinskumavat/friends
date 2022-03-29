eeb907586f6607a967cc0cefe85fb921516ae684
Nitin Kumavat <nitin.s.kumavat@gmail.com>
2022-03-30 01:49:54 +0530

    removed data from js

--- src/data.js
+++ src/data.js
@@ -5,83 +5,9 @@ export const data = [
     fav: 1,
   },
   {
-    name: "Hello",
+    name: "Akash",
     id: 2,
     fav: 0,
   },
-  // {
-  //     "name": "Hi",
-  //     "id": 3,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "No",
-  //     "id": 4,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "rdfs No",
-  //     "id": 5,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "No sfs",
-  //     "id": 6,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 7,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 8,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 9,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 10,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 11,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 12,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 13,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 14,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 15,
-  //     "fav": 0
-  // },
-  // {
-  //     "name": "Nsfo",
-  //     "id": 16,
-  //     "fav": 0
-  // },
-  {
-    name: "Nsfo",
-    id: 17,
-    fav: 0,
-  },
+
 ];
